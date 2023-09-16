67795d866974227f39dae37ee7e1ce5627d51930
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-09-16 06:29:32 +0100

    Проверка ДЗ | 8. Операторы управления циклом
    - Правильное:
    - Неправильное:
    - Улучшены:
    - На доработку: №7

--- tasks/task_7.py
+++ tasks/task_7.py
@@ -5,6 +5,8 @@ player_2_name = input("Игрок 2, введите свое имя: ")
 variables = ["камень", "ножницы", "бумага"]
 
 congratulatory_message = "Поздравляем! Победитель -"
+warning_msg = "ПРЕДУПРЕЖДЕНИЕ:"
+rules_msg = "соблюдайте правила игры. Начнем игру заново!"
 
 while True:
     print()
@@ -19,27 +21,43 @@ while True:
         print(congratulatory_message, player_2_name + "!")
     elif player_2_ans == "камень" and player_1_ans == "бумага":
         print(congratulatory_message, player_1_name + "!")
-
     elif player_1_ans == "ножницы" and player_2_ans == "бумага":
         print(congratulatory_message, player_1_name + "!")
     elif player_2_ans == "ножницы" and player_1_ans == "бумага":
         print(congratulatory_message, player_2_name + "!")
 
-    elif player_1_ans not in variables and\
-            player_2_ans not in variables:
-        print("ПРЕДУПРЕЖДЕНИЕ: Пожалуйста соблюдайте"
-              " правила игры. Начнем игру заново!")
+    # Лучше использовать скобки для переноса
+    elif (player_1_ans not in variables
+          and player_2_ans not in variables):
+        print(warning_msg, "Пожалуйста", rules_msg)
     elif player_1_ans not in variables:
-        print(player_1_name, "соблюдайте правила игры."
-                             " Начнем игру заново!")
+        print(player_1_name, rules_msg)
     elif player_2_ans not in variables:
-        print(player_2_name, "соблюдайте правила игры."
-                             " Начнем игру заново!")
-
+        print(player_2_name, rules_msg)
     elif player_1_ans == player_2_ans:
         print("Ничья! Продолжайте играть")
+
     if player_1_ans in variables and player_2_ans in variables:
         ext = input("Хотите сыграть еще раз? (да/нет): ")
         if ext == "нет":
             print("До встречи!")
             break
+
+# есть баг, когда ничья программа запрашивает,
+# хотим ли продолжит игру, так не должно быть
+
+"""
+Добро пожаловать в игру "Камень-Ножницы-Бумага"!
+Игрок 1, введите свое имя: Анна
+Игрок 2, введите свое имя: Макс
+
+Анна: бумага
+Макс: камень
+Поздравляем! Победитель - Анна!
+Хотите сыграть еще раз? (да/нет): да
+
+Анна: камень
+Макс: камень
+Ничья! Продолжайте играть
+Хотите сыграть еще раз? (да/нет): 
+"""
